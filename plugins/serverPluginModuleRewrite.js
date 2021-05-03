6ba8121fd6464eb38bfc573a4041569dcebc62fc
yuccie <yuccie1617@gmail.com>
2021-05-03 23:53:11 +0800

    增加es-module-lexer解析esmodule语法,以及magic-string重写模块路径

--- plugins/serverPluginModuleRewrite.js
+++ plugins/serverPluginModuleRewrite.js
@@ -1,4 +1,31 @@
 const { readBody } = require('../utils.js');
+const { parse } = require('es-module-lexer'); 
+const MagicString = require('magic-string');
+
+function rewriteImports(source) {
+    const imports = parse(source)[0];
+    console.log('imports', imports);
+    // 返回类似如下结果，数组的每一项都是一条import语法，s就是导入模块的开始
+    // end就是字符串的结束，n就是具体的模块
+    // [
+    //     [
+    //       { n: 'vue', s: 27, e: 30, ss: 0, se: 31, d: -1 },
+    //       { n: './App.vue', s: 49, e: 58, ss: 32, se: 59, d: -1 }
+    //     ],
+    //     [],
+    //     false
+    // ]
+
+    // 需要重写字符串，这里需要用到magic-string
+    let str = new MagicString(source);
+    imports.forEach(({ n,s,e }) => {
+        if (/^[^\/\.]/.test(n)) {
+            const id = `/node_modules/.vite/${n}.js?v=${Date.now()}`;
+            str.overwrite(s, e, id);
+        }
+    })
+    return String(str);
+}
 
 
 exports.moduleRewritePlugin = async function({ app, root }) {
@@ -6,15 +33,19 @@ exports.moduleRewritePlugin = async function({ app, root }) {
         // 洋葱模型，这里执行权直接交给下一个中间件
         await next();
 
-        // 等到上面的中间件（其实就是静态文件服务器）执行完，会执行下面的逻辑
-        // static中间件执行完会在ctx.body上挂载资源文件，这里通过一个方法读取
-        if (ctx.body) {
+        // 我们需要拦截.js文件，
+        if (ctx.body && ctx.response.is('js')) {
             // 将公共方法抽离出去，注意这里的数据是流，而且是异步
             const resBody =  await readBody(ctx.body);
-            // 刷新一下页面，就会看到resBody的内容为html里的内容
-            console.log('resbody', resBody);
+            // 刷新一下页面，就会看到main.js文件里的内容
+            // console.log('resbody', resBody);
+
+            // 解析import语法，重写文件路径
+            // 'vue' -> '/node_modules/.vite/vue.js?v=4fd96829'
+            // 需要用到es-module-lexer 这是 ES Module 语法的词法分析利器，比babel和Acorn强大许多
+            const res = rewriteImports(resBody);
+            // 重写后挂载在ctx.body上
+            ctx.body = res;
         }
-        const resBody =  await readBody(ctx.body);
-        console.log('resbody', resBody);
     })
 }
